38357dda3539c69a19bd7dbc9ab9b21ecb31ede1
locovna <locovna@gmail.com>
2017-04-12 15:50:25 +0300

    delete secondary unnecessary loop

--- wmappy/app/src/main/java/com/locovna/wmappy/controller/QueryUtils.java
+++ wmappy/app/src/main/java/com/locovna/wmappy/controller/QueryUtils.java
@@ -21,7 +21,7 @@ public class QueryUtils {
   //  public static final String REQUEST_URL = "https://raw.githubusercontent.com/David-Haim/CountriesToCitiesJSON/master/countriesToCities.json";
   public static final String TEST_JSON_RESPONCE = "{\n" +
       "  \"China\": [\"fendou\", \"hetao\"],\n" +
-      "  \"Japan:\" : [\"tokyo\", \"hiroshima\"]\n" +
+      "  \"Japan\" : [\"tokyo\", \"hiroshima\"]\n" +
       "}";
 
   /**
@@ -37,27 +37,25 @@ public class QueryUtils {
     try {
       JSONObject obj = new JSONObject(TEST_JSON_RESPONCE);
 
-      for (int i = 0; i < obj.length(); i++) {
+      // parse every key in JSONObject
+      Iterator<String> keys = obj.keys();
+      while (keys.hasNext()) {
+        Log.v(LOG_TAG, "keys: " + keys);
+        String countryName = (String) keys.next();
+        JSONArray cities = obj.getJSONArray(countryName);
 
-        // parse every key in JSONObject
-        Iterator<String> keys = obj.keys();
-        while (keys.hasNext()) {
-          String countryName = (String) keys.next();
-          JSONArray cities = obj.getJSONArray(countryName);
-
-          // convert JSONArray to ArrayList<String>
-          ArrayList<String> citiesArray = new ArrayList<String>();
-          if (cities != null) {
-            int len = cities.length();
-            for (int x = 0; x < len; x++) {
-              citiesArray.add(cities.get(i).toString());
-            }
+        // convert JSONArray to ArrayList<String>
+        ArrayList<String> citiesArray = new ArrayList<String>();
+        if (cities != null) {
+          int len = cities.length();
+          for (int i = 0; i < len; i++) {
+            citiesArray.add(cities.get(i).toString());
           }
-
-          Country country = new Country(countryName, citiesArray);
-          countries.add(country);
-          Log.v(LOG_TAG, country.getCountry() + country.getCities());
         }
+
+        Country country = new Country(countryName, citiesArray);
+        countries.add(country);
+        Log.v(LOG_TAG, country.getCountry() + country.getCities());
       }
 
     } catch (JSONException e) {
