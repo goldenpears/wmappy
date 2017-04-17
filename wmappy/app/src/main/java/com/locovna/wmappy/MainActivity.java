098ab30fb0058831a0154399f5efb0133d7b7633
locovna <locovna@gmail.com>
2017-04-17 13:25:41 +0300

    Fetch data via network request

--- wmappy/app/src/main/java/com/locovna/wmappy/MainActivity.java
+++ wmappy/app/src/main/java/com/locovna/wmappy/MainActivity.java
@@ -1,5 +1,6 @@
 package com.locovna.wmappy;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
@@ -9,26 +10,50 @@ import com.locovna.wmappy.controller.QueryUtils;
 import com.locovna.wmappy.model.Country;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
   public static final String LOG_TAG = MainActivity.class.getSimpleName();
+  public static final String REQUEST_URL =
+      "https://raw.githubusercontent.com/David-Haim/CountriesToCitiesJSON/master/countriesToCities.json";
+  private CountryAdapter mAdapter;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    ArrayList<Country> countries = QueryUtils.extractCountries();
-
-    //    ArrayList<Country> countries = new ArrayList<>();
-    //    countries.add(new Country("USA"));
-    //    countries.add(new Country("Canada"));
-    //    countries.add(new Country("Some Canada"));
-    //    countries.add(new Country("Any Japan"));
-
     ListView countriesListView = (ListView) findViewById(R.id.list);
 
-    CountryAdapter adapter = new CountryAdapter(this, countries);
-    countriesListView.setAdapter(adapter);
+    mAdapter = new CountryAdapter(this, new ArrayList<Country>());
+    countriesListView.setAdapter(mAdapter);
+
+    CountryAsyncTask task = new CountryAsyncTask();
+    task.execute(REQUEST_URL);
+  }
+
+  private class CountryAsyncTask extends AsyncTask<String, Void, List<Country>> {
+    @Override
+    protected List<Country> doInBackground(String... urls) {
+      // Don't perform the request if there are no URLs, or the first URL is null
+      if (urls.length < 1 || urls[0] == null) {
+        return null;
+      }
+
+      List<Country> result = QueryUtils.fetchCountriesData(urls[0]);
+      return result;
+    }
+
+    @Override
+    protected void onPostExecute(List<Country> data) {
+      // Clear the adapter of previous earthquake data
+      mAdapter.clear();
+
+      // If there is a valid list of {@link Earthquake}s, then add them to the adapter's
+      // data set. This will trigger the ListView to update.
+      if (data != null && !data.isEmpty()) {
+        mAdapter.addAll(data);
+      }
+    }
   }
 }
--- wmappy/app/src/main/java/com/locovna/wmappy/controller/QueryUtils.java
+++ wmappy/app/src/main/java/com/locovna/wmappy/controller/QueryUtils.java
@@ -1,5 +1,6 @@
 package com.locovna.wmappy.controller;
 
+import android.text.TextUtils;
 import android.util.Log;
 
 import com.locovna.wmappy.model.Country;
@@ -8,8 +9,17 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * Helper methods related to requesting and receiving country data from JSON
@@ -18,11 +28,10 @@ import java.util.Iterator;
 public class QueryUtils {
 
   public static final String LOG_TAG = QueryUtils.class.getSimpleName();
-  //  public static final String REQUEST_URL = "https://raw.githubusercontent.com/David-Haim/CountriesToCitiesJSON/master/countriesToCities.json";
-  public static final String TEST_JSON_RESPONCE = "{\n" +
-      "  \"China\": [\"fendou\", \"hetao\"],\n" +
-      "  \"Japan\" : [\"tokyo\", \"hiroshima\"]\n" +
-      "}";
+  //  public static final String TEST_JSON_RESPONCE = "{\n" +
+  //      "  \"China\": [\"fendou\", \"hetao\"],\n" +
+  //      "  \"Japan\" : [\"tokyo\", \"hiroshima\"]\n" +
+  //      "}";
 
   /**
    * No one should create {@link QueryUtils},
@@ -31,11 +40,107 @@ public class QueryUtils {
   private QueryUtils() {
   }
 
-  public static ArrayList<Country> extractCountries() {
-    ArrayList<Country> countries = new ArrayList<>();
+  public static List<Country> fetchCountriesData(String requestUrl) {
+    // Create URL object
+    URL url = createUrl(requestUrl);
+
+    // Perform HTTP request to the URL and receive a JSON response back
+    String jsonResponse = null;
+    try {
+      jsonResponse = makeHttpRequest(url);
+    } catch (IOException e) {
+      Log.e(LOG_TAG, "Problem making the HTTP request.", e);
+    }
+
+    // Extract relevant fields from the JSON response and create a list of {@link Countries}s
+    List<Country> countries = extractDataFromJson(jsonResponse);
+
+    // Return the list of {@link Countries}
+    return countries;
+  }
+
+  private static URL createUrl(String stringUrl) {
+    URL url = null;
+    try {
+      url = new URL(stringUrl);
+    } catch (MalformedURLException e) {
+      Log.e(LOG_TAG, "Problem building the URL ", e);
+    }
+    return url;
+  }
+
+  /**
+   * Make an HTTP request to the given URL and return a String as the response.
+   */
+  private static String makeHttpRequest(URL url) throws IOException {
+    String jsonResponse = "";
+
+    // If the URL is null, then return early.
+    if (url == null) {
+      return jsonResponse;
+    }
+
+    HttpURLConnection urlConnection = null;
+    InputStream inputStream = null;
+    try {
+      urlConnection = (HttpURLConnection) url.openConnection();
+      urlConnection.setReadTimeout(10000 /* milliseconds */);
+      urlConnection.setConnectTimeout(15000 /* milliseconds */);
+      urlConnection.setRequestMethod("GET");
+      urlConnection.connect();
+
+      // If the request was successful (response code 200),
+      // then read the input stream and parse the response.
+      if (urlConnection.getResponseCode() == 200) {
+        inputStream = urlConnection.getInputStream();
+        jsonResponse = readFromStream(inputStream);
+      } else {
+        Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+      }
+    } catch (IOException e) {
+      Log.e(LOG_TAG, "Problem retrieving the earthquake JSON results.", e);
+    } finally {
+      if (urlConnection != null) {
+        urlConnection.disconnect();
+      }
+      if (inputStream != null) {
+        // Closing the input stream could throw an IOException, which is why
+        // the makeHttpRequest(URL url) method signature specifies than an IOException
+        // could be thrown.
+        inputStream.close();
+      }
+    }
+    return jsonResponse;
+  }
+
+  /**
+   * Convert the {@link InputStream} into a String which contains the
+   * whole JSON response from the server.
+   */
+  private static String readFromStream(InputStream inputStream) throws IOException {
+    StringBuilder output = new StringBuilder();
+    if (inputStream != null) {
+      InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+      BufferedReader reader = new BufferedReader(inputStreamReader);
+      String line = reader.readLine();
+      while (line != null) {
+        output.append(line);
+        line = reader.readLine();
+      }
+    }
+    return output.toString();
+  }
+
+
+  public static List<Country> extractDataFromJson(String countriesJson) {
+    if (TextUtils.isEmpty(countriesJson)) {
+      return null;
+    }
+
+    List<Country> countries = new ArrayList<>();
 
     try {
-      JSONObject obj = new JSONObject(TEST_JSON_RESPONCE);
+      JSONObject obj = new JSONObject(countriesJson);
 
       // parse every key in JSONObject
       Iterator<String> keys = obj.keys();
@@ -58,7 +163,10 @@ public class QueryUtils {
         Log.v(LOG_TAG, country.getCountry() + country.getCities());
       }
 
-    } catch (JSONException e) {
+    } catch (
+        JSONException e)
+
+    {
       Log.e(LOG_TAG, "Problem parsing the countries JSON results", e);
     }
 
